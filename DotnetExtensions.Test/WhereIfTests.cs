351030471f7f63ce15ec6f1bba8d665da9e4752a
Douglas Lopes <douglasfls@live.com>
2025-01-13 23:11:30 +0000

    initial commit

---
+++ DotnetExtensions.Test/WhereIfTests.cs
@@ -0,0 +1,148 @@
+﻿using System.Linq.Expressions;
+using FluentAssertions;
+
+namespace DotnetExtensions.Test;
+
+public class WhereIfTests
+{
+    private static Func<int, bool> IsEven => i => i % 2 == 0;
+    private static Expression<Func<int, bool>> IsEvenExpr => i => i % 2 == 0;
+
+    [Theory]
+    [InlineData(1, 10, true, 5)]
+    [InlineData(1, 1, true, 0)]
+    [InlineData(1, 10, false, 10)]
+    [InlineData(1, 1, false, 1)]
+    public void GetEven_WhereIfShould_QueryIfConditionIsSet(int value, int rangeSize, bool condition, int expected)
+    {
+        Enumerable.Range(1, rangeSize)
+            .WhereIf(condition, IsEven)
+            .Should()
+            .HaveCount(expected);
+    }
+
+    [Theory]
+    [InlineData(1, 10, true, 5)]
+    [InlineData(1, 1, true, 0)]
+    [InlineData(1, 10, false, 10)]
+    [InlineData(1, 1, false, 1)]
+    [InlineData(1, 10, null, 10)]
+    [InlineData(1, 1, null, 1)]
+    public void GetEven_WhereIfShould_QueryIfConditionIsNull(int value, int rangeSize, bool? condition, int expected)
+    {
+        Enumerable.Range(1, rangeSize)
+            .WhereIf(condition, IsEven)
+            .Should()
+            .HaveCount(expected);
+    }
+
+    [Theory]
+    [InlineData(1, 10, true, 5)]
+    [InlineData(1, 1, true, 0)]
+    [InlineData(1, 10, false, 10)]
+    [InlineData(1, 1, false, 1)]
+    public void GetEven_QueryableWhereIfShould_QueryIfConditionIsSet(int value, int rangeSize, bool condition,
+        int expected)
+    {
+        Enumerable.Range(1, rangeSize)
+            .AsQueryable()
+            .WhereIf(condition, IsEvenExpr)
+            .Should()
+            .HaveCount(expected);
+    }
+
+    [Theory]
+    [InlineData(1, 10, true, 5)]
+    [InlineData(1, 1, true, 0)]
+    [InlineData(1, 10, false, 10)]
+    [InlineData(1, 1, false, 1)]
+    [InlineData(1, 10, null, 10)]
+    [InlineData(1, 1, null, 1)]
+    public void GetEven_QueryableWhereIfShould_QueryIfConditionIsNull(int value, int rangeSize, bool? condition,
+        int expected)
+    {
+        Enumerable.Range(1, rangeSize)
+            .AsQueryable()
+            .WhereIf(condition, IsEvenExpr)
+            .Should()
+            .HaveCount(expected);
+    }
+
+    [Fact]
+    public void MergeDictionaries_ShouldCreateAthirdDictionaryWithMergedValues()
+    {
+        var first = new Dictionary<string, string>()
+        {
+            { "1", "10" },
+            { "2", "20" },
+            { "3", "30" },
+        };
+        var second = new Dictionary<string, string>()
+        {
+            { "3", "300" },
+            { "4", "400" },
+        };
+        
+        var third = first.Merge(second);
+        
+        third
+            .Should()
+            .ContainKeys(["1", "2", "3", "4"]);
+        
+        third["3"]
+            .Should()
+            .Be("300");
+    }
+    
+    [Fact]
+    public void MergeDictionaries_ShouldCreateAthirdDictionaryWithMergedValues1()
+    {
+        var first = new Dictionary<string, string>()
+        {
+            { "a", "10" },
+            { "b", "20" },
+            { "c", "30" },
+        };
+        var second = new Dictionary<string, string>()
+        {
+            { "C", "300" },
+            { "d", "400" },
+        };
+        
+        var third = first.Merge(second, StringComparer.OrdinalIgnoreCase);
+        
+        third
+            .Should()
+            .ContainKeys(["a", "b", "c", "d"]);
+        
+        third["c"]
+            .Should()
+            .Be("300");
+    }
+    
+    [Fact]
+    public void MergeDictionaries_ShouldCreateAthirdDictionaryWithMergedValues2()
+    {
+        var first = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "a", "10" },
+            { "b", "20" },
+            { "c", "30" },
+        };
+        var second = new Dictionary<string, string>()
+        {
+            { "C", "300" },
+            { "d", "400" },
+        };
+        
+        var third = first.Merge(second);
+        
+        third
+            .Should()
+            .ContainKeys(["a", "b", "c", "d"]);
+        
+        third["c"]
+            .Should()
+            .Be("300");
+    }
+}
---
+++ DotnetExtensions/LinqExtensions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DotnetExtensions
+{
+    public static class LinqExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    action(enumerator.Current);
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> WhereIf<TResult>
+        (this IEnumerable<TResult> values, bool condition, Func<TResult, bool> predicateIf,
+            Func<TResult, bool> predicateElse = null)
+            => condition ? values?.Where(predicateIf)
+                : predicateElse != null ? values?.Where(predicateElse)
+                : values;
+
+        public static IEnumerable<TResult> WhereIf<TResult>
+        (this IEnumerable<TResult> values, bool condition, Func<TResult, int, bool> predicateIf,
+            Func<TResult, int, bool> predicateElse = null)
+            => condition ? values?.Where(predicateIf)
+                : predicateElse != null ? values?.Where(predicateElse)
+                : values;
+
+        public static IEnumerable<TResult> WhereIf<TResult>
+        (this IEnumerable<TResult> values, bool condition, Func<TResult, int, bool> predicateIf,
+            Func<TResult, bool> predicateElse = null)
+            => condition ? values?.Where(predicateIf)
+                : predicateElse != null ? values?.Where(predicateElse)
+                : values;
+
+        public static IEnumerable<TResult> WhereIf<TResult>
+        (this IEnumerable<TResult> values, bool condition, Func<TResult, bool> predicateIf,
+            Func<TResult, int, bool> predicateElse)
+            => condition ? values?.Where(predicateIf)
+                : predicateElse != null ? values?.Where(predicateElse)
+                : values;
+
+        public static IEnumerable<TResult> WhereIf<TResult>
+        (this IEnumerable<TResult> values, bool? condition, Func<TResult, bool> predicateIf,
+            Func<TResult, bool> predicateElse = null)
+            => condition.HasValue && condition.Value ? values?.Where(predicateIf)
+                : predicateElse != null ? values?.Where(predicateElse)
+                : values;
+
+        public static IQueryable<TResult> WhereIf<TResult>
+        (this IQueryable<TResult> values, bool condition, Expression<Func<TResult, bool>> predicateIf,
+            Expression<Func<TResult, bool>> predicateElse = null)
+            => condition ? values?.Where(predicateIf)
+                : predicateElse != null ? values?.Where(predicateElse)
+                : values;
+
+        public static IQueryable<TResult> WhereIf<TResult>
+        (this IQueryable<TResult> values, bool? condition, Expression<Func<TResult, bool>> predicateIf,
+            Expression<Func<TResult, bool>> predicateElse = null)
+            => condition.HasValue && condition.Value ? values?.Where(predicateIf)
+                : predicateElse != null ? values?.Where(predicateElse)
+                : values;
+
+        public static IDictionary<TKey, TValue> Merge<TKey, TValue>(this IDictionary<TKey, TValue> first,
+            IDictionary<TKey, TValue> second, IEqualityComparer<TKey> comparer = null)
+        {
+            if (first is Dictionary<TKey, TValue> dictionary
+                && comparer is null)
+                comparer = dictionary.Comparer;
+            
+            var merged = new Dictionary<TKey, TValue>(comparer);
+            if (first != null)
+            {
+                foreach (var kvp in first)
+                {
+                    merged[kvp.Key] = kvp.Value;
+                }
+            }
+            if (second != null)
+            {
+                foreach (var value in second)
+                {
+                    merged[value.Key] = value.Value;
+                }
+            }
+
+            return merged;
+        }
+    }
+}
